c266bc65ca818e1dbb038d9339634ffbbab4a4a7
Maximus-Beauchamp <maximus.beauchamp@gmail.com>
2024-04-03 12:44:23 -0400

    changed logic to conditionally render hall details to remove clutter

--- src/Pages/MealPlanningPage.js
+++ src/Pages/MealPlanningPage.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import DiningHall from '../Components/DiningHall';
+import DiningHallDetails from '../Components/DiningHallDetails'; // Adjust the import path as necessary
 import diningHallsData from '../Components/MealItems';
 import { getMealJournal, addMealToJournal, MealJournalDisplay } from '../Components/MealJournalService'; 
 import Notification from '../Components/Notification';
 
-
 const MealPlanningPage = () => {
   const [mealJournal, setMealJournal] = useState(getMealJournal());
   const [showNotification, setShowNotification] = useState(false);
-  const [visibleHall, setVisibleHall] = useState(null);
+  const [selectedHall, setSelectedHall] = useState(null);
 
   useEffect(() => {
-    // Refresh the meal journal from localStorage when the component mounts
     setMealJournal(getMealJournal());
   }, []);
 
@@ -23,19 +21,34 @@ const MealPlanningPage = () => {
     });
   };
 
+  const handleHallSelection = (hallName) => {
+    const hall = diningHallsData.find(hall => hall.name === hallName);
+    setSelectedHall(hall);
+  };
+
+  const handleBackToList = () => {
+    setSelectedHall(null);
+  };
+
   return (
     <div>
-      <h1>Meal Planning</h1>
-      {diningHallsData.map((hall, index) => (
-        <div key={index} style={{ margin: '10px', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
-          <h2 onClick={() => setVisibleHall(visibleHall === hall.name ? null : hall.name)} style={{ cursor: 'pointer' }}>
-            {hall.name}
-          </h2>
-          {visibleHall === hall.name && (
-            <DiningHall hallDetails={hall} addMealToJournal={addMealToJournalHandler} />
-          )}
-        </div>
-      ))}
+      {selectedHall ? (
+        <>
+          <button onClick={handleBackToList} style={{ margin: '10px', padding: '5px' }}>Back to Halls List</button>
+          <DiningHallDetails hallDetails={selectedHall} addMealToJournal={addMealToJournalHandler} />
+        </>
+      ) : (
+        <>
+          <h1>Meal Planning</h1>
+          {diningHallsData.map((hall, index) => (
+            <div key={index} style={{ margin: '10px', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }}>
+              <h2 onClick={() => handleHallSelection(hall.name)} style={{ cursor: 'pointer' }}>
+                {hall.name}
+              </h2>
+            </div>
+          ))}
+        </>
+      )}
       <Notification message="Food Logged" isVisible={showNotification} />
       <MealJournalDisplay mealJournal={mealJournal} />
     </div>
